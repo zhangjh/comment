5852c4c7e2fa9a75f325d881085bb6ee417da8b2
zhangjh <njhxzhangjihong@126.com>
2018-04-02 11:51:01 -0400

    接口增加跨域

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://zhangjh.me");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/comment',commentRouter);
